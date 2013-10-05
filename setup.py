e9c538f521142ee7b902d4eabf018b8194195f58
Matvey Kruglov <kubuzzzz@gmail.com>
2013-10-05 13:48:32 +0400

    Initial package structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os, sys
+sys.path.insert(0, 'src')
+import motorchan
+
+requires = [
+    'tornado',
+]
+
+setup(
+    name='motorchan',
+    version=motorchan.__version__,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=requires,
+    zip_safe=False,
+    entry_points={
+        'console_scripts': [
+            'motorchan = motorchan:run',
+        ],
+    },
+)
