3a508f64255c2b4d60d204f0456cfa8bc03edb9b
zzzombat <zzzombat@gmail.com>
2013-10-05 19:31:42 +0400

    Add some forms

--- src/motorchan/forms.py
+++ src/motorchan/forms.py
@@ -14,3 +14,14 @@ class LoginForm(forms.Form):
 class BoardForm(forms.Form):
     slug = fields.StringField(u'Идентификатор', [required_validator])
     name = fields.StringField(u'Имя', [required_validator])
+
+
+class PostForm(forms.Form):
+    name = fields.StringField(u'Имя')
+    email = fields.StringField(u'Email')
+    body = fields.TextField(u'Тело', [required_validator])
+    image = fields.FileField(u'Изображение')
+
+
+class ThreadForm(PostForm):
+    image = fields.FileField(u'Изображение', [required_validator])
